ff94800599f7d665d2c215268af562b827ec1835
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2022-09-29 12:08:34 +0100

    pause and play functionality

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import styles from './SongList.module.css'
 import List from '../List'
 
 function SongList() {
-  return (
-    <div>
-        {
-          List.map((list) => (
-              <div key={list.id}>
-                  <h1>{list.name}</h1>
-                  <h3>{list.title}</h3>
-                  <img src={list.image} alt={list.name}/>
-              </div>
-          ))
-        }
-    </div>
-  )
+    let [play, setPlay] = useState(false)
+    let [isPlaying, setIsPlaying] = useState(null)
+    let [active, setActive] = useState([])
+    // let [content , setContent] = useState([])
+    // let ctrl = !isPlaying ? <p>&#9658;</p> : <p>&#8214;</p>
+    const audio = useRef(null);
+    return (
+        <div>
+            {
+                List.map((list) => {
+                    return (
+                        <div key={list.id}>
+                            <div
+                                onClick={() => {
+                                    const song = list.song;
+                                    setActive(list)
+                                    if (play === song) {
+                                        isPlaying ? setActive(null) : setActive(list)
+                                        isPlaying ? audio.current.pause() : audio.current.play();
+                                        setIsPlaying(!isPlaying);
+                                    } else {
+                                        if (audio.current) {
+                                            audio.current.pause();
+                                        }
+                                        setPlay(song);
+                                        setIsPlaying(true);
+                                        audio.current = new Audio(song);
+                                        audio.current.play();
+                                    }
+                                }}>
+                                <div className={`${active === list ? styles.active : styles.notActive}`}></div>
+                            </div>
+                            <h1>{list.name}</h1>
+                            <h3>{list.title}</h3>
+                            <img src={list.image} alt={list.name} />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default SongList
